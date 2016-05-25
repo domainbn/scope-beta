7b5bc63cc81bb5655d68e73a8dee25f59b2d25ff
naqiuddinihsan <naqiuddin.ihsan@gmail.com>
2016-05-25 22:41:28 +0800

    SQL connection test

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
+    $servername = "ec2-50-17-255-6.compute-1.amazonaws.com";
+    $username = "pmhppusmuzyuqu";
+    $password = "9JzIAdE8ZTJa8MCRTYYZ6_XXVC";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+    echo "Connected successfully";
+
+
     echo 'test';
+    
 ?>
