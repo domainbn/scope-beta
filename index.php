a8172d40cfff688e501b1d712dc4d8755447bae1
naqiuddinihsan <naqiuddin.ihsan@gmail.com>
2016-05-31 14:26:46 +0800

    echo "Test" test

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <?php
-    $servername = "ec2-50-17-255-6.compute-1.amazonaws.com";
-    $username = "pmhppusmuzyuqu";
-    $password = "9JzIAdE8ZTJa8MCRTYYZ6_XXVC";
-
-    // Create connection
-    $conn = new mysqli($servername, $username, $password);
-
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    } 
+//    $servername = "ec2-50-17-255-6.compute-1.amazonaws.com";
+//    $username = "pmhppusmuzyuqu";
+//    $password = "9JzIAdE8ZTJa8MCRTYYZ6_XXVC";
+//    $database = "df0h35apt2cc0q";
+//
+//    $con = mysqli_connect($servername,$username,$password,$database);
+//
+//// Check connection
+//if (mysqli_connect_errno())
+//  {
+//  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+//  }
 
     echo 'test';
 ?>
