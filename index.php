6bbfef533658adf6714962c1605bb187141c08b7
naqiuddinihsan <naqiuddin.ihsan@gmail.com>
2016-05-31 14:17:21 +0800

    Reversion to commit 3

--- index.php
+++ index.php
@@ -10,11 +10,6 @@
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     } 
-    echo "Connected successfully";
 
-
-    echo 'Website works';
-//    echo $conn;
-//    echo $servername;
-    
+    echo 'test';
 ?>
