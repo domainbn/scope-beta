0dd068ea98aa64aa28ad4108599cf2c28a60d192
naqiuddinihsan <naqiuddin.ihsan@gmail.com>
2016-05-31 14:30:41 +0800

    pg_connect test

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-//    $servername = "ec2-50-17-255-6.compute-1.amazonaws.com";
+//    $host = "ec2-50-17-255-6.compute-1.amazonaws.com";
 //    $username = "pmhppusmuzyuqu";
 //    $password = "9JzIAdE8ZTJa8MCRTYYZ6_XXVC";
 //    $database = "df0h35apt2cc0q";
@@ -11,6 +11,18 @@
 //  {
 //  echo "Failed to connect to MySQL: " . mysqli_connect_error();
 //  }
+
+    $host        = "host=ec2-50-17-255-6.compute-1.amazonaws.com";
+    $port        = "port=5432";
+   $dbname      = "dbname=df0h35apt2cc0q";
+   $credentials = "user=pmhppusmuzyuqu password=9JzIAdE8ZTJa8MCRTYYZ6_XXVC";
+
+   $db = pg_connect( "$host $port $dbname $credentials"  );
+   if(!$db){
+      echo "Error : Unable to open database\n";
+   } else {
+      echo "Opened database successfully\n";
+   }
 
     echo 'test';
 ?>
